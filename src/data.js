5ca013b99bb87734fb5e924565621d95f0fa8355
Pavithra0706 <egpavithra07@gmail.com>
2023-11-16 18:56:36 +0530

    changes

---
+++ src/data.js
@@ -0,0 +1,98 @@
+const dataList = [
+  {
+    img: 'https://tse3.mm.bing.net/th?id=OIP.iWsHU_6Lw7Ss3zTt95gQZgHaD8&pid=Api&P=0&h=180',
+    name: "MongoDB",
+    description: "MongoDB works on concept of collection and document the data. This MongoDB Tutorial is designed for both beginners and experienced professionals. It aims to guide you through the fundamentals and help you get started with MongoDB, a flexible document-oriented database. ",
+    id: "Full stack"
+  },
+  {
+    img: 'https://tse4.mm.bing.net/th?id=OIF.8GqgbYYkLbjE67PVujgYYg&pid=Api&P=0&h=180',
+    name: "The Role of Continuous Learning in Tech Hiring",
+    description: "It is the power of continuous learning and Development. Not just during the process of hiring, but even after hiring, the onboarding and internal training process is more efficient in various ways for employers that hire pre-trained, certified, and continually learning job aspirants.",
+    id: "Career"
+  },
+  {
+    img: 'https://tse3.mm.bing.net/th?id=OIP.EFF61WNWIoOsGILSMZfQEwHaEc&pid=Api&P=0&h=180',
+    name: "Cyber Security",
+    description: "Cyber Security is the body of technologies, processes, and practices designed to protect networks, devices, programs, and data from attack, theft, damage, modification or unauthorized access. ",
+    id: "Cyber security"
+  },
+  
+  {
+    img: 'https://tse4.mm.bing.net/th?id=OIP.fRTGQX2uvC889MsWttdLoQHaDV&pid=Api&P=0&h=180',
+    name: "Machine learning",
+    description: "Machine Learning is the field of study that gives computers the capability to learn without being explicitly programmed. ML is one of the most exciting technologies that one would have ever come across. ",
+    id: "Data Science"
+  },
+  
+  {
+    img: 'https://tse4.mm.bing.net/th?id=OIP.Z6QOiDI2THs6d5qOzPmGkAHaEK&pid=Api&P=0&h=180',
+    name: "Node.js",
+    description: "Node.js is an open-source and cross-platform JavaScript runtime environment. It is a popular tool for almost any kind of project! ",
+    id: "Full stack"
+  },
+  
+  
+  {
+    img: 'https://tse2.mm.bing.net/th?id=OIP.MYlusmK6l5c8Ufx9CebnSQHaE7&pid=Api&P=0&h=180',
+    name: "Bootstrap",
+    description: "Bootstrap is an open-source framework used for creating responsive, mobile-first sites. We know that’s a mouthful, so let’s go through the parts of that definition one by one.",
+    id: "Full stack"
+  },
+  {
+    img: 'https://tse4.mm.bing.net/th?id=OIP.bmSeC4euZNd1wfgZAxTiIQHaFA&pid=Api&P=0&h=180',
+    name: "Data analytics",
+    description: "Data analytics converts raw data into actionable insights.",
+    id: "Data Science"
+  },
+  {
+    img: 'https://tse3.mm.bing.net/th?id=OIP.eWUjOCd3NX0YumGQSDNQAwHaEK&pid=Api&P=0&h=180',
+    name: "React Js",
+    description: "React Js Js is li globally used Front-end JS framework and is popular with both software developers and project sponsors.",
+    id: "Full stack"
+  },
+  {
+    img: 'https://tse2.mm.bing.net/th?id=OIP.iPNT4mfd9vNxvtr4EgGHOwHaFj&pid=Api&P=0&h=180',
+    name: "Developer vs Tester",
+    description: "In the world of making computer programs and apps, there are two important jobs: developers and testers. Developers are the builders; they create the code that makes everything work. Testers are detectives; they carefully check the code to make sure it’s good and works well. ",
+    id: "Career"
+  },
+  {
+    img: 'https://tse1.mm.bing.net/th?id=OIP.Pxpu72aTn1bTJyn53mivQgHaEo&pid=Api&P=0&h=180',
+    name: "Javascript",
+    description: "JavaScript, a versatile high-level programming language, serves as the backbone of interactive web development. With its primary application on the client side, JavaScript empowers developers to imbue web pages with dynamic functionality, responding to user actions seamlessly. ",
+    id: "Full stack"
+  },
+  
+  
+  {
+    img: 'https://tse1.mm.bing.net/th?id=OIP.l6p8sCR_M9w0FTKOL8sk6wHaD4&pid=Api&P=0&h=180',
+    name: "Keras",
+    description: "Keras is li high-level, deep learning API developed by Google for implementing neural networks.",
+    id: "Data Science"
+  },
+  {
+    img: 'https://tse3.mm.bing.net/th?id=OIP.G86pBh5Xh1bNu5gGPze2LwHaEK&pid=Api&P=0&h=180',
+    name: "HTML & CSS",
+    description: "HTML and CSS are scripting languages used to create li web page and web applications.",
+    id: "Full stack"
+  },
+  
+ 
+  {
+    img: 'https://tse2.mm.bing.net/th?id=OIP.7Sl_8kKhNYXx16Ydu7mFXAHaDn&pid=Api&P=0&h=180',
+    name: "Best Product-Based Companies for Web Developers Freshers [2023]",
+    description: "Every one of us wants to get into the top product-based companies and when we talk about web development, which is a high-demanding profession in the IT industry, getting into the top Product-Based Companies for Web Developers adds value to your resume and your working experience.",
+    id: "Career"
+  },
+  
+  {
+    img: 'https://tse3.mm.bing.net/th?id=OIP.RojzB10vSz9un-B91XS7SgHaEb&pid=Api&P=0&h=180',
+    name: "Artificial Intelligence vs Human Intelligence",
+    description: "One of the hot topics that’s been trending these days is that artificial intelligence will replace human intelligence and make us lose our jobs. This has been the trend for the last couple of years as AI innovations like ChatGPT were made public. As people started using it extensively, there has been a growing concern that Artificial Intelligence will slowly take over human intelligence.",
+    id: "career"
+  },
+
+  
+]
+export {dataList}
